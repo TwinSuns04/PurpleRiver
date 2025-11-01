633184a44d0da1f5521ba0d6fc596f8e3c04fe01
TwinSuns04 <gbills-gamedev@tuta.com>
2025-10-31 23:59:55 -0600

    Started programming flora spawner gd script. Still unsure why C++ version crashes gdextension and Godot.

--- purple-river/Map/Flora/flora_spawner.gd
+++ purple-river/Map/Flora/flora_spawner.gd
@@ -1,7 +1,31 @@
 extends Node
 
+@export var randSeedVal = 1
+@export var spawnPoint: Vector2i
+@export var spawnAreaSize: Vector2
+@onready var rectColl = $CollisionShape2D
+
 func _ready():
-	pass
+	rectColl.shape.extents = spawnAreaSize
+	$CollisionShape2D/Marker2D.position = Vector2(0,0)
 	
 func _process(delta: float):
+	pass
+
+func gen_spawn_point():
+	var randomness = RandomNumberGenerator.new()
+	randomness.randomize()
+	
+	var areaBoundsPos: Vector2i
+	var areaBoundsNeg: Vector2i
+	areaBoundsPos.x = spawnAreaSize.x / 2
+	areaBoundsNeg.x = -areaBoundsPos.x
+	areaBoundsPos.y = spawnAreaSize.y / 2
+	areaBoundsNeg.y = -areaBoundsPos.y
+	
+	spawnPoint.x = randi_range(areaBoundsNeg.x, areaBoundsPos.x)
+	spawnPoint.y = randi_range(areaBoundsNeg.y, areaBoundsNeg.y)
+	var x_coord: int
+
+func place_flora():
 	pass
