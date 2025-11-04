d83eabe248e2c1c4153efacb12a11d49f1659962
TwinSuns04 <gbills-gamedev@tuta.com>
2025-11-03 21:35:59 -0700

    Created core flora spawner and required scenes and scripts

--- purple-river/Map/Flora/flora_spawner.gd
+++ purple-river/Map/Flora/flora_spawner.gd
@@ -3,13 +3,20 @@ extends Node
 @export var randSeedVal = 1
 @export var spawnPoint: Vector2i
 @export var spawnAreaSize: Vector2
+@export var spawnDelay: float
+@export var floraType: int
 @onready var rectColl = $CollisionShape2D
 
 func _ready():
 	rectColl.shape.extents = spawnAreaSize
 	$CollisionShape2D/Marker2D.position = Vector2(0,0)
+	$SpawnTimer.wait_time = spawnDelay
+	flora_type()
 	
 func _process(delta: float):
+	pass
+
+func flora_type(): # choose which flora scene to instance
 	pass
 
 func gen_spawn_point():
@@ -25,7 +32,10 @@ func gen_spawn_point():
 	
 	spawnPoint.x = randi_range(areaBoundsNeg.x, areaBoundsPos.x)
 	spawnPoint.y = randi_range(areaBoundsNeg.y, areaBoundsNeg.y)
+	print("spawnPoint: ", spawnPoint)
+	$CollisionShape2D/Marker2D/Sprite2D.position = spawnPoint
 	var x_coord: int
 
 func place_flora():
-	pass
+	gen_spawn_point()
+	
