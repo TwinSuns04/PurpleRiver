b59c9997245890e05308713b49447904fc761f7b
TwinSuns04 <gbills-gamedev@tuta.com>
2025-11-03 22:42:08 -0700

    Created HaikuSystem.h

---
+++ src/mechanics/haiku/haikuSystem.h
@@ -0,0 +1,20 @@
+#ifndef HAIKUSYSTEM_H
+#define HAIKUSYSTEM_H
+
+#include <godot_cpp/classes/node.hpp>
+
+namespace godot {
+
+    class HaikuSystem : public Node {
+        GDCLASS(HaikuSystem, Node)
+
+    private:
+
+    protected:
+
+    public:
+
+    };
+}
+
+#endif
